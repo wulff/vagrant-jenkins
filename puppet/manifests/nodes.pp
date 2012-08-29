61e3672b7f45d010459104f8bb872fe7fb3d7396
wulff <wulff@ratatosk.net>
2012-08-29 15:07:32 +0200

    added basic puppet manifests

---
+++ puppet/manifests/nodes.pp
@@ -0,0 +1,9 @@
+# node definitions
+
+# the default node configures the machine for use as a jenkins server
+node default {
+  # virtual framebuffer for running selenium tests using a headless firefox
+  package { ['xvfb', 'x11-apps', 'xfonts-100dpi', 'xfonts-75dpi', 'xfonts-scalable', 'xfonts-cyrillic']:
+    ensure => present,
+  }
+}
